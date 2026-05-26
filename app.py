81f915160ec68b9057766aea67116fe1dc0371ae
Mukesh <mukesh.bunny999@gmail.com>
2026-05-26 11:47:46 +0400

    Switch to Hostinger SMTP hello@shivyaspaces.com

--- app.py
+++ app.py
@@ -24,18 +24,17 @@ limiter = Limiter(
 # -----------------------------------------------
 # EMAIL CONFIG — change these to your Gmail details
 # -----------------------------------------------
-app.config["MAIL_SERVER"]   = "smtp.gmail.com"
+app.config["MAIL_SERVER"]   = "smtp.hostinger.com"
 app.config["MAIL_PORT"]     = 587
 app.config["MAIL_USE_TLS"]  = True
-app.config["MAIL_USERNAME"] = "mukesh.bunny999@gmail.com"
-app.config["MAIL_PASSWORD"] = "aunflvjqjbdzefus"
-app.config["MAIL_DEFAULT_SENDER"] = ("ShivyaSpaces", "mukesh.bunny999@gmail.com")
+app.config["MAIL_USERNAME"] = "hello@shivyaspaces.com"
+app.config["MAIL_PASSWORD"] = "Shivya@768"
+app.config["MAIL_DEFAULT_SENDER"] = ("ShivyaSpaces", "hello@shivyaspaces.com")
 
 # -----------------------------------------------
 # SITE URL — change this after hosting
 # -----------------------------------------------
-#SITE_URL = "http://127.0.0.1:5000"
-SITE_URL = "https://www.shivyaspaces.com"
+SITE_URL = "http://127.0.0.1:5000"
 mail = Mail(app)
 
 
